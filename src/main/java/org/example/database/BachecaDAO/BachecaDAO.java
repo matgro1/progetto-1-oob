9a640d7f22debc15842384b88bee069427a15787
matgro1 <matgro1234@outlook.it>
2026-01-26 14:14:12 +0100

    a

--- src/main/java/org/example/database/BachecaDAO/BachecaDAO.java
+++ src/main/java/org/example/database/BachecaDAO/BachecaDAO.java
@@ -1,7 +1,7 @@
 package org.example.database.BachecaDAO;
 import org.example.model.Bacheca;
 import java.util.List;
-
+//
 public interface BachecaDAO {
     Bacheca save(Bacheca bacheca);
     Bacheca findById(int id);
