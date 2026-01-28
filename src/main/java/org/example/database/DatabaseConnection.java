a12da86ed494239a6ad8b0526e29659130ce91e4
matgro1 <matgro1234@outlook.it>
2026-01-28 15:08:17 +0100

    Refactor package structure for DAO classes; move to 'org.example.dao' and update related imports

--- src/main/java/org/example/database/DatabaseConnection.java
+++ src/main/java/org/example/database/DatabaseConnection.java
@@ -10,7 +10,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/your_database";
+    private static final String URL = "jdbc:postgresql://localhost:5432/progetto programmazione ad oggetti";
     private static final String USER = "postgres";
     private static final String PASSWORD = "";
     public static final BachecaDAOImpl bachecaDB = new BachecaDAOImpl();
